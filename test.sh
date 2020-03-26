2a6012df97e7516afc9aff86b9de5a13da51bc0d
td936595 <tushar.das3445@gmail.com>
2020-03-25 21:52:02 -0500

    Inserting my first sh script

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "hello world"
+a=12
+b=15
+total=$((a+b))
+echo The total is $total
+
+echo Please type your name
+read name
+echo Welcome to the program $name
+
+truth=$1
+if [ $truth -lt 10 ]
+then
+        echo "One digit number"
+#elif [[ ( $truth -gt 10 && $truth -lt 100 ) ]]
+        #echo "Two digit number"
+else
+        echo "Three digit number"
+fi
+
+for (( counter=$truth; counter>0; counter-- ))
+do
+
+        echo -n " $counter "
+done
+printf "\n"
